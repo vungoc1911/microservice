c4d9d982ab3f21e91d471ea60dff1d4a39e2cc89
NgocVT20 <vungoc20092001@g,ail.com>
2023-10-17 23:37:30 +0700

    api-gateway

---
+++ discovery-service/src/main/java/com/programming/techie/discoveryservice/config/SecurityConfig.java
@@ -0,0 +1,9 @@
+package com.programming.techie.discoveryservice.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSe {
+}
