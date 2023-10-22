a100a43fd6e533bf20333d48a63b2f4b8ebcb734
NgocVT20 <vungoc20092001@g,ail.com>
2023-10-22 23:12:42 +0700

    api-gateway

--- discovery-service/src/main/java/com/programming/techie/discoveryservice/config/SecurityConfig.java
+++ discovery-service/src/main/java/com/programming/techie/discoveryservice/config/SecurityConfig.java
@@ -1,9 +1,15 @@
 package com.programming.techie.discoveryservice.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
-@EnableWebSecurity
-public class SecurityConfig extends WebSe {
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.csrf().ignoringRequestMatchers("/eureka/**");
+        return httpSecurity.build();
+    }
 }
